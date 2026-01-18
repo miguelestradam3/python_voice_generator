b4be46df2004da12868b59a0a0a6109b0e284e27
miguelestradam36 <miguelestradam36@gmail.com>
2026-01-18 15:01:07 -0600

    Reviewing the project

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
+from modules.voicegen import RobVoiceGen
+
 if __name__  == "__main__":
-    from modules.voicegen import RobVoiceGen
     buffer = RobVoiceGen()
     buffer.voice_conf = 0
     buffer.voice_rate_conf = -25
+    
+    #Modify the current text attribute if you already have the string (text)
+    buffer.current_text = 'Hello world'
+
+    #Use this setter if you have a txt file
+    #buffer.text_script_conf = r'../scripts/text.txt'
+
     buffer.output_conf = r'../output/example.mp3'
-    buffer.text_script_conf = r'../scripts/text.txt'
+    
     #buffer.run_speech()
     #buffer.voices_try_out()
     buffer.save_to_mp3()
